0d783a2e0fafa0b4552287e248dac9e5fbd32f9d
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 12:39:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require "spec_helper"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  names = []
+  data.each do |details, value| #=> iterates through whole hash
+  value.each do |info, arr|
+      arr.each do |name|
+        if !name.include?(name)
+            names << name 
+      end
+    end
+  end
+  names.each do |namez|
+    pigeon_list[namez] = Hash.new {|k, v| k[v] = []}
+    data.each do |details, value|
+      pigeon_list[namez][details]
+      value.each do |info, arr|
+        arr.each do |name|
+          if name == namez 
+            pigeon_list[name][details] << info.to_s 
+        end
+      end
+  end
+  end
+  pigeon_list
 end
