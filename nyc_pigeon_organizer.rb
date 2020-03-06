526b96f651b8ac2961f3027d5b7f0d6731afc53c
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 13:29:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "spec_helper"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  names = []
+  data.each do |key, value| #=> iterates through whole hash
+  value.each do |info, arr|
+      arr.each do |name|
+        if pigeon_list.has_key?(name)
+              if pigeon_list[name].has_key?(key)
+            pigeon_list[name][key] << info.to_s
+          else
+            pigeon_list[name][key] = [info.to_s]
+      end
+    else
+      pigeon_list[name] = {key => [info.to_s]}
+    end
+  end
+end
+end
+pigeon_list
 end
